d726b6c9504bbd64a9d6cde3e22437105decf435
thach76 <117991019+thach76@users.noreply.github.com>
2023-11-24 10:13:51 +0700

    Add files via upload

---
+++ MaHoaGSA/MaHoaGSA/Program.cs
@@ -0,0 +1,211 @@
+﻿using System;
+using System.Text;
+using System.Numerics;
+using System.Reflection;
+using System.Security.Cryptography.X509Certificates;
+
+namespace MaHoaRSA
+{
+    public class Key
+    {
+        public int k1 { get; set; }
+        public int k2 { get; set; }
+        public Key() { }
+
+    }
+    internal class Program
+    {
+        public static int ModN(int n, int m)
+        {
+            if (n >= 0)
+            {
+                return n % 26;
+            }
+            else
+            {
+                return (n + m * ((int)-n/m + 1)) % m;
+            }
+        }
+
+        public static int EuclidMoRong(int a, int m)
+        {
+            for (int i = 2; i < m; i++)
+            {
+                if ((a * i) % m == 1)
+                {
+                    return i;
+                }
+            }
+            return 0;
+        }
+
+        public static int TimUCLN(int a, int b)
+        {
+            while (b != 0)
+            {
+                int temp = b;
+                b = a % b;
+                a = temp;
+            }
+            return a;
+        }
+
+
+        // Hàm kiểm tra số nguyên tố
+        static bool KiemTraSoNguyenTo(int n)
+        {
+            // Kiểm tra nếu số n là số nguyên tố
+            if (n <= 1)
+                return false; // Nếu n là số âm hoặc bằng 1, không phải là số nguyên tố
+            if (n <= 3)
+                return true;  // Nếu n là 2 hoặc 3, là số nguyên tố
+
+            if (n % 2 == 0 || n % 3 == 0)
+                return false; // Nếu n chia hết cho 2 hoặc 3, không phải là số nguyên tố
+
+            for (int i = 5; i * i <= n; i += 6)
+            {
+                if (n % i == 0 || n % (i + 2) == 0)
+                    return false; // Nếu n chia hết cho i hoặc i + 2, không phải là số nguyên tố
+            }
+
+            return true; // Nếu không có số nào từ 2 đến căn bậc hai của n chia hết cho n, thì n là số nguyên tố
+        }
+
+        public static void TaoKey(Key publicKey, Key privateKey)
+        {
+            int p = 17, q = 7;
+            do
+            {
+                Console.WriteLine("Nhập vào 2 số p, q: ");
+                p = int.Parse(Console.ReadLine());
+                q = int.Parse(Console.ReadLine());
+            }
+            while (!(KiemTraSoNguyenTo(p) && KiemTraSoNguyenTo(q)));
+
+            int N = p * q;
+            int OmegaN = (p - 1) * (q - 1);
+            int e = 0;
+
+            for (int i = 6; i < OmegaN; i++)
+            {
+                if (TimUCLN(i, OmegaN) == 1)
+                {
+                    e = i;
+                    break;
+                }
+            }
+            //if (p==17)
+            e = 13;
+
+            int d = EuclidMoRong(e, OmegaN);
+
+            //Console.WriteLine("N là: " + N);
+            //Console.WriteLine("O(N) là: " + OmegaN);
+            Console.WriteLine("e là: " + e);
+            Console.WriteLine("d là: " + d);
+
+            
+            publicKey.k1 = e;
+            publicKey.k2 = N;
+            
+            privateKey.k1 = d;
+            privateKey.k2 = N;
+        }
+
+        public static int powMod (int n, int m,int modulus)
+        {
+            int result = 1;
+
+            for (int i = 0; i < m; i++)
+            {
+                result = (result * n) % modulus;
+            }
+            return result;
+        }
+
+        public static string MaHoa(Key key, string input)
+        {
+            string output = "";
+            for (int i = 0; i<input.Length; i++)
+            {
+                int T = input[i];
+                int c =  powMod(T, key.k1, key.k2);
+                output += " " + c;
+            }
+            return output;
+        }
+
+        public static string GiaiMa(Key privateKey, string input)
+        {
+            string output = "";
+            // Sử dụng phương thức Split để tách chuỗi thành mảng các từ
+            string[] words = input.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+
+            // Khai báo một mảng số nguyên
+            int[] numbers = new int[words.Length];
+
+            // Chuyển đổi từng từ thành số nguyên và lưu vào mảng numbers
+            for (int i = 0; i < words.Length; i++)
+            {
+                if (int.TryParse(words[i], out int num))
+                {
+                    numbers[i] = num;
+                }
+                else
+                {
+                    // Xử lý trường hợp không thể chuyển đổi thành số nguyên
+                    Console.WriteLine($"Không thể chuyển đổi từ '{words[i]}' thành số nguyên.");
+                }
+            }
+
+            
+            foreach (int num in numbers)
+            {
+                char c = (char)powMod(num, privateKey.k1, privateKey.k2);
+                output += c;
+            }
+
+            return output;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            Key publicKey = new Key();
+            Key privateKey = new Key();
+            TaoKey(publicKey, privateKey);
+
+            Console.WriteLine("Vui lòng chọn mã hóa hoặc giải mã: ");
+            Console.WriteLine("1. Mã Hóa");
+            Console.WriteLine("2. Giải Mã");
+            Console.WriteLine("3. Thoát");
+            int chon = 0;
+            do
+            {
+                chon = int.Parse(Console.ReadLine());
+                switch (chon)
+                {
+                    case 1:
+                        Console.WriteLine("Vui lòng nhập văn bản để mã hóa: ");
+                        string txt = Console.ReadLine();
+                        string maHoa = MaHoa(publicKey, txt);
+                        Console.WriteLine("văn bản đã được mã hóa thành: " + maHoa);
+                        break;
+                    case 2:
+                        Console.WriteLine("Vui lòng nhập văn bản để giải mã: ");
+                        string txt2 = Console.ReadLine();
+                        string giaiMa = GiaiMa(privateKey, txt2);
+                        Console.WriteLine("văn bản đã được giải mã thành: " + giaiMa);
+                        break;
+                    default: break;
+
+                }
+            }
+            while (chon != 1 || chon != 2);
+
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ MaHoaHiu/MaHoaHiu/Program.cs
@@ -0,0 +1,409 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MaHoaHiu
+{
+    class MaTran
+    {
+        public int[] phanTu = new int[10];
+        public int capMaTran = new int();
+        public MaTran()
+        {
+            capMaTran = 2;
+            for (int i = 0; i < 10; i++)
+            {
+                phanTu[i] = 0;
+            }
+        }
+
+        public MaTran(int capMaTran)
+        {
+            this.capMaTran = capMaTran;
+            for (int i = 0; i < 10; i++)
+            {
+                phanTu[i] = 0;
+            }
+        }
+
+        
+
+        public int Det()
+        {
+            if (capMaTran == 2)
+            {
+                return phanTu[0] * phanTu[3] - phanTu[1] * phanTu[2];
+            }
+            else if (capMaTran == 3)
+            {
+                return phanTu[0] * phanTu[4] * phanTu[8] + phanTu[1] * phanTu[5] * phanTu[6] + phanTu[2] * phanTu[3] * phanTu[7]
+                        - phanTu[2] * phanTu[4] * phanTu[6] - phanTu[0] * phanTu[5] * phanTu[7] - phanTu[1] * phanTu[3] * phanTu[8];
+            }
+            else
+            {
+                return 0;
+            }
+        }
+
+        public MaTran NhanVoiSo(int num)
+        {
+            MaTran B = new MaTran(3);
+            B.capMaTran = capMaTran;
+            for (int i = 0; i < capMaTran * capMaTran; i++)
+            {
+                B.phanTu[i] = phanTu[i] * num;
+            }
+            return B;
+
+        }
+
+        public MaTran NhanVoi12(MaTran A)
+        {
+            MaTran B = new MaTran(2);
+            if (capMaTran == 2)
+            {
+                B.phanTu[0] = A.phanTu[0] * phanTu[0] + A.phanTu[1] * phanTu[1];
+                B.phanTu[1] = A.phanTu[0] * phanTu[2] + A.phanTu[1] * phanTu[3];
+            }
+            
+            return B;
+        }
+
+        public MaTran NhanVoi13(MaTran A)
+        {
+            MaTran B = new MaTran(3);
+            if (capMaTran == 3)
+            {
+                B.phanTu[0] = A.phanTu[0] * phanTu[0] + A.phanTu[1] * phanTu[3] + A.phanTu[2] * phanTu[6];
+                B.phanTu[1] = A.phanTu[0] * phanTu[1] + A.phanTu[1] * phanTu[4] + A.phanTu[2] * phanTu[7];
+                B.phanTu[2] = A.phanTu[0] * phanTu[2] + A.phanTu[1] * phanTu[5] + A.phanTu[2] * phanTu[8];
+            }
+            return B;
+        }
+
+        public MaTran Adj()
+        {
+            MaTran B = new MaTran(3);
+            if (capMaTran == 2)
+            {
+                B.phanTu[0] = phanTu[3];
+                B.phanTu[1] = -phanTu[1];
+                B.phanTu[2] = -phanTu[2];
+                B.phanTu[3] = phanTu[0];
+                B.capMaTran = 2;
+            }
+            else if (capMaTran == 3)
+            {
+                B.phanTu[0] = phanTu[4] * phanTu[8] - phanTu[5] * phanTu[7];
+                B.phanTu[3] = phanTu[5] * phanTu[6] - phanTu[3] * phanTu[8];
+                B.phanTu[6] = phanTu[3] * phanTu[7] - phanTu[4] * phanTu[6];
+                B.phanTu[1] = phanTu[7] * phanTu[2] - phanTu[8] * phanTu[1];
+                B.phanTu[4] = phanTu[8] * phanTu[0] - phanTu[6] * phanTu[2];
+                B.phanTu[7] = phanTu[6] * phanTu[1] - phanTu[7] * phanTu[0];
+                B.phanTu[2] = phanTu[1] * phanTu[5] - phanTu[2] * phanTu[4];
+                B.phanTu[5] = phanTu[2] * phanTu[3] - phanTu[0] * phanTu[5];
+                B.phanTu[8] = phanTu[0] * phanTu[4] - phanTu[1] * phanTu[3];
+            }
+            return B;
+        }
+
+        public void inMaTran(int number)
+        {
+            for (int i = 0; i< number; i++)
+            {
+                Console.Write(phanTu[i] + "\t");
+                if ((i+1) % capMaTran  == 0)
+                {
+                    Console.WriteLine();
+                }
+            }
+        }
+
+        public void nhapMaTran()
+        {
+            for (int i = 0; i < capMaTran * capMaTran; i++)
+            {
+                Console.Write("Nhap Phan Tu " + (i+1) + ": " );
+                phanTu[i] = int.Parse(Console.ReadLine());
+            }
+        }
+
+        public static int ModToB(int a, int b)
+        {
+            if (a >= 0)
+                return a % b;
+            else
+            {
+                int c = (int)-a / b;
+                return a + c * b + b;
+            }
+        }
+
+        public MaTran Mod(int num)
+        {
+            MaTran B = new MaTran(3);
+            B.capMaTran = capMaTran;
+            for (int i = 0; i < capMaTran * capMaTran; i++)
+            {
+                B.phanTu[i] = ModToB(phanTu[i], num);
+            }
+            return B;
+        }
+
+        public string ChuyenTxt()
+        {
+            string txt = "";
+            for (int i = 0; i < capMaTran; i++)
+            {
+                char character = (char)phanTu[i];
+                character += 'a';
+                txt += character.ToString();
+            }
+            return txt;
+        }
+
+
+    }
+    internal class Program
+    {
+        public static MaTran[] chiaMaTran(MaTran[] maTran, string text, int soMT, int capMaTran)
+        {
+            //int soMT = (int) text.Length / capMaTran;
+            int leng = 0;
+
+            //MaTran[] maTran = new MaTran[1000];
+
+            for (int i = 0; i < soMT; i++)
+            {
+                //maTran[i].capMaTran = capMaTran;
+                maTran[i] = new MaTran(capMaTran);
+                if (capMaTran == 2)
+                {
+                    maTran[i].phanTu[0] = text[leng++] - 'a';
+                    maTran[i].phanTu[1] = text[leng++] - 'a';
+                }
+                else if (capMaTran == 3)
+                {
+                    maTran[i].phanTu[0] = text[leng++] - 'a';
+                    maTran[i].phanTu[1] = text[leng++] - 'a';
+                    maTran[i].phanTu[2] = text[leng++] - 'a';
+                }
+            }
+
+            if (text.Length % capMaTran != 0)
+            {
+                //maTran[soMT].capMaTran = capMaTran;
+                maTran[soMT] = new MaTran(capMaTran);
+                if (capMaTran == 2)
+                {
+                    maTran[soMT].phanTu[0] = text[leng++] - 'a';
+                    maTran[soMT].phanTu[1] = 'x' - 'a';
+                }
+                else if (capMaTran == 3)
+                {
+                    maTran[soMT].phanTu[0] = text[leng++] - 'a';
+                    if (text.Length % capMaTran == 1)
+                    {
+                        maTran[soMT].phanTu[1] = 'x' - 'a';
+                        maTran[soMT].phanTu[2] = 'x' - 'a';
+                    }
+                    else
+                    {
+                        maTran[soMT].phanTu[1] = text[leng++] - 'a';
+                        maTran[soMT].phanTu[2] = 'x' - 'a';
+                    }
+
+                }
+            }
+
+            return maTran;
+        }
+
+        public static int ModToB(int a, int b)
+        {
+            if (a >= 0)
+                return a % b;
+            else
+            {
+                int c = (int)-a / b;
+                return a + c * b + b;
+            }
+        }
+        public static int EuclidMoRong(int a, int m)
+        {
+            for (int i = 1; i < m; i++)
+            {
+                if (ModToB(a * i, m) == 1)
+                {
+                    return i;
+                }
+            }
+            return 0;
+        }
+
+
+
+
+        public static void MaHoaHiu(MaTran key, int capMaTran)
+        {
+            
+
+        }
+
+        public static void GiaiMaHiu()
+        {
+
+        }
+
+        static string InsertCharacter(string input, char characterToAdd, int positionToAdd)
+        {
+            if (positionToAdd < 0 || positionToAdd > input.Length)
+            {
+                throw new ArgumentOutOfRangeException("positionToAdd", "Vị trí thêm không hợp lệ.");
+            }
+
+            // Sử dụng Substring để chia chuỗi thành hai phần và sau đó nối ký tự vào giữa
+            string output = input.Substring(0, positionToAdd) + characterToAdd + input.Substring(positionToAdd);
+
+            return output;
+        }
+
+        static string CombineWithSpaces(string input1, string input2)
+        {
+
+            // Tạo một chuỗi mới để lưu trữ kết quả
+            int k = 0;
+
+            for (int i = 0; i < input1.Length; i++)
+            {
+                if (input1[i] == ' ')
+                {
+                    input2 = InsertCharacter(input2, ' ', i );
+                    k++;
+                }
+            }
+
+            return input2;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            Console.WriteLine("Mã hóa Hiu vui lòng chọn mã hóa hoặc giải mã");
+            Console.WriteLine("1. Mã Hóa   ");
+            Console.WriteLine("2. Giải Mã  ");
+            int luaChon = int.Parse(Console.ReadLine());
+            Console.WriteLine("Vui lòng nhâp vào 1 chuỗi: ");
+            string input = Console.ReadLine();
+            string input2 = input.Replace(" ", "");
+            Console.WriteLine("Vui lòng nhập vào cấp ma trận: ");
+            int capMaTran = int.Parse(Console.ReadLine());
+            Console.WriteLine("Vui lòng nhập key: ");
+            MaTran key = new MaTran(capMaTran);
+            MaTran keyGiaiMa = new MaTran(capMaTran);
+            key.nhapMaTran();
+            /*            key.phanTu[0] = 17;
+                        key.phanTu[1] = 17;
+                        key.phanTu[2] = 5;
+                        key.phanTu[3] = 21;
+
+                        key.phanTu[4] = 18;
+                        key.phanTu[5] = 21;
+                        key.phanTu[6] = 2;
+                        key.phanTu[7] = 2;
+                        key.phanTu[8] = 19;*/
+
+            /*key.phanTu[0] = 16;
+            key.phanTu[1] = 15;
+            key.phanTu[2] = 5;
+            key.phanTu[3] = 20;
+
+            key.phanTu[4] = 19;
+            key.phanTu[5] = 21;
+            key.phanTu[6] = 3;
+            key.phanTu[7] = 4;
+            key.phanTu[8] = 18;*/
+            int soMT = input2.Length / capMaTran;
+            MaTran[] chiaMT = new MaTran[soMT + 1];
+            MaTran[] maHoa = new MaTran[soMT + 1];
+            MaTran[] giaiMa = new MaTran[soMT + 1];
+
+            for (int i = 0; i < soMT + 1; i++)
+            {
+                chiaMT[i] = new MaTran(capMaTran);
+                maHoa[i] = new MaTran(capMaTran);
+                giaiMa[i] = new MaTran(capMaTran);
+            }
+            
+            chiaMaTran(chiaMT, input2, soMT, capMaTran);
+            int num = EuclidMoRong(ModToB(key.Det(), 26), 26);
+            //Console.WriteLine(ModToB(key.Det(), 26));
+            keyGiaiMa = key.Adj().NhanVoiSo(num).Mod(26);
+            if (num == 0)
+            {
+                Console.WriteLine("Key bạn nhập có vấn đề vui lòng kiểm tra lại!!");
+                
+            }
+            string strMaHoa = "";
+            string strGiaiMa = "";
+            do
+            {
+                switch (luaChon)
+                {
+
+                    //mã hóa
+                    case 1:
+                        strMaHoa = "";
+                        if (input2.Length % capMaTran != 0)
+                            soMT++;
+                        for (int i = 0; i < soMT; i++)
+                        {
+
+                            if (capMaTran == 2)
+                                maHoa[i] = key.NhanVoi12(chiaMT[i]);
+                            if (capMaTran == 3)
+                                maHoa[i] = key.NhanVoi13(chiaMT[i]);
+
+                            maHoa[i] = maHoa[i].Mod(26);
+                            strMaHoa += maHoa[i].ChuyenTxt();
+
+                        }
+                        strMaHoa = CombineWithSpaces(input, strMaHoa);
+                        Console.WriteLine("mã hóa: " + strMaHoa);
+                        break;
+                    case 2:
+                        strGiaiMa = "";
+                        if (input2.Length % capMaTran != 0)
+                            soMT++;
+                        for (int i = 0; i < soMT; i++)
+                        {
+
+                            if (capMaTran == 2)
+                                maHoa[i] = keyGiaiMa.NhanVoi12(chiaMT[i]);
+                            if (capMaTran == 3)
+                                maHoa[i] = keyGiaiMa.NhanVoi13(chiaMT[i]);
+
+                            maHoa[i] = maHoa[i].Mod(26);
+                            strGiaiMa += maHoa[i].ChuyenTxt();
+                            maHoa[i].inMaTran(3);
+                        }
+                        strGiaiMa = CombineWithSpaces(input, strGiaiMa);
+                        Console.WriteLine("giải mã: " +strGiaiMa);
+                        break;
+
+                    default: break;
+                }
+            }
+            while (!(luaChon == 1 || luaChon == 2));
+            
+            
+
+
+            //MaHoaHiu();
+            Console.ReadKey();
+        }
+    }
+}
